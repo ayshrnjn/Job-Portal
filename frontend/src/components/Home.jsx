d62aabd2e85efa9b232b6f8112551a4c112fec11
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2024-12-26 03:30:50 +0530

    home page carousel

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CatergoryCarousel from './CatergoryCarousel'
+import LatestJobs from './LatestJobs'
 
 function Home() {
   return (
@@ -9,8 +10,8 @@ function Home() {
         <Navbar/>  
         <HeroSection/>
          <CatergoryCarousel/>
-        {/* <LatestJobs/>
-        <Footer/>  */}
+        <LatestJobs/>
+        {/* // <Footer/>  */}
        
     </div>
   )
