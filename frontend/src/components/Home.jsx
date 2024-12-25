0f16e330c26bee46d88392f60929c473ecad4f12
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2024-12-26 02:28:42 +0530

    home page started
    herosection

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CatergoryCarousel from './CatergoryCarousel'
 
 function Home() {
   return (
     <div>
         <Navbar/>  
+        <HeroSection/>
+         <CatergoryCarousel/>
+        {/* <LatestJobs/>
+        <Footer/>  */}
        
     </div>
   )
