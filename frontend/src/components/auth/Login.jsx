d82e2f789d15cc1a7a4a000b57cd50aa866c4bd0
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2024-12-27 14:18:21 +0530

    login and profile
    when we login then profile picture will show this link we created

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -9,7 +9,7 @@ import { toast } from 'sonner';
 import axios from 'axios';
 import { USER_API_END_POINT } from '@/utils/constant';
 import { useDispatch, useSelector } from 'react-redux';
-import { setLoading } from '@/redux/authSlice';
+import { setLoading, setUser } from '@/redux/authSlice';
 import { Loader2 } from 'lucide-react';
 
 function Login() {
@@ -39,6 +39,7 @@ function Login() {
       });
 
       if (res.data.success) {
+        dispatch(setUser(res.data.user));
         navigate("/");  // Navigate inside submitHandler
         toast.success(res.data.message);
       }
