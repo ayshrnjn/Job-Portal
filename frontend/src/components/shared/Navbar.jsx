5e46e3c89604ccbfe415dc504b700ea49b058895
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2025-01-11 17:02:20 +0530

    admin side

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -14,28 +14,28 @@ import { setUser } from '@/redux/authSlice';
 function Navbar() {
 
 
-   
-  
-  const {user}=useSelector(store=>store.auth);
-  const dispatch=useDispatch();
-  const navigate =useNavigate();
 
-  const logoutHandler= async()=>{
-    try{
-     
-      const res= await axios.get(`${USER_API_END_POINT}/logout`,{withCredentials:true});
 
-      if(res.data.success){
+  const { user } = useSelector(store => store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    try {
+
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+
+      if (res.data.success) {
         dispatch(setUser(null));
         navigate("/");
         toast.success(res.data.message)
 
       }
     }
-    catch(error){
+    catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
-      
+
     }
   }
 
@@ -49,16 +49,30 @@ function Navbar() {
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/jobs">Jobs</Link> </li>
-            <li> <Link to="/browse">Browse</Link> </li>
-            
+
+
+            {
+              user && user.role === 'recruiter' ? (
+                <>
+                  <li><Link to="/admin/companies">Companies</Link></li>
+                  <li><Link to="/admin/jobs">Jobs</Link></li>
+                </>
+              ) : (
+                <>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/jobs">Jobs</Link></li>
+                  <li><Link to="/browse">Browse</Link></li>
+                </>
+              )
+            }
+
+
           </ul>
           {!user ? (
             <div className="flex items-center gap-2">
-             <Link to="/login"> <Button variant="outline">Login </Button>  </Link>
+              <Link to="/login"> <Button variant="outline">Login </Button>  </Link>
 
-              <Link to ="/signup"><Button className="bg-purple-600 hover:bg-purple-700">Signup</Button></Link>
+              <Link to="/signup"><Button className="bg-purple-600 hover:bg-purple-700">Signup</Button></Link>
             </div>
           ) : (
             <Popover>
@@ -87,13 +101,17 @@ function Navbar() {
                   </div>
                 </div>
                 <div className="flex flex-col text-gray-600">
-                  <div className="flex w-fit items-center gap-2 cursor-pointer">
-                    <User2 />
-                    <Button variant="link"><Link to="/profile"> View Profile</Link></Button>
-                  </div>
+                  {
+                    user && user.role === 'student' && (
+                      <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                        <User2 />
+                        <Button variant="link"> <Link to="/profile">View Profile</Link></Button>
+                      </div>
+                    )
+                  }
                   <div className="flex w-fit items-center gap-2 cursor-pointer">
                     <LogOut />
-                    <Button  onClick ={logoutHandler}variant="link">Log Out</Button>
+                    <Button onClick={logoutHandler} variant="link">Log Out</Button>
                   </div>
                 </div>
               </PopoverContent>
