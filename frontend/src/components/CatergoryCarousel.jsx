6e35962c3252dfe752426df442f01c60c7a99b99
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2025-01-15 14:23:57 +0530

    filtering of jobs

--- frontend/src/components/CatergoryCarousel.jsx
+++ frontend/src/components/CatergoryCarousel.jsx
@@ -3,6 +3,7 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import { Button } from './ui/button';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setSearchedQuery } from '@/redux/jobSlice';
 
 
 const category = [
@@ -15,6 +16,14 @@ const category = [
 
 
 const CatergoryCarousel = ()=> {
+       
+    const dispatch =useDispatch();
+    const navigate=useNavigate();
+
+    const searchJobHandler=(query)=>{
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+     }
     return (
         <div>
             <Carousel className="w-full max-w-xl mx-auto my-20">
@@ -22,7 +31,7 @@ const CatergoryCarousel = ()=> {
                     {
                         category.map((cat, index) => (
                             <CarouselItem className="md:basis-1/2 lg-basis-1/3">
-                                <Button variant="outline" className="rounded-full">{cat}</Button>
+                                <Button  onClick ={()=>searchJobHandler(cat)}variant="outline" className="rounded-full">{cat}</Button>
                             </CarouselItem>
                         ))
                     }
